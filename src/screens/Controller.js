9a733fb8e5c474bef2191166f47b8178a49faf99
sanjaypgit <93530564+sanjaypgit@users.noreply.github.com>
2022-11-15 19:31:14 +0530

    IP Address Updated
    IP Address Updated

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://44.202.60.179:8080/api/v1/";
+    this.baseUrl = "http://3.85.91.116:8080/api/v1/";
   }
   render(){
     return(
